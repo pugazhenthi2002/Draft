fd40a7c91d48a409836d35ce571f444425a440c5
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2024-03-18 12:42:45 +0530

    Add files via upload

---
+++ SampleSql/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DatabaseLibrary;
+using MySql.Data.MySqlClient;
+
+namespace SampleSql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //string connectionstring = "server=localhost;port=3306;uid=root;" + "database=practice";
+            manager = new MySqlHandler("localhost", "root", "Lucid123", "ProjectManagement");
+            var x = manager.Connect().Result;
+            var z = manager.ShowTables("expense").Value;
+            var y = manager.CheckConnection().ToString();
+        }
+        DatabaseManager manager;
+    }
+}
